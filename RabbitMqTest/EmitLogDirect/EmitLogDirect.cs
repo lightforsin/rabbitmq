275adcbcbba1e6cd88fcb5b99c0f84d97779af01
BordeaP <paul.bordea@verivox.com>
2020-04-02 17:38:32 +0300

    Add scenario for routing messages

---
+++ RabbitMqTest/EmitLogDirect/EmitLogDirect.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace EmitLogDirect
+{
+    internal class EmitLogDirect
+    {
+        private const string ExchangeName = "direct_logs";
+
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using var channel = connection.CreateModel();
+                channel.ExchangeDeclare(exchange: ExchangeName, type: ExchangeType.Direct);
+
+                var severity = (args.Length > 0) ? args[0] : "info";
+
+                var message = (args.Length > 1)
+                    ? string.Join(" ", args.Skip(1).ToArray())
+                    : "Hello World!";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "direct_logs",
+                    routingKey: severity,
+                    basicProperties: null,
+                    body: body);
+
+                Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args, int index)
+        {
+            return ((args.Length > 0)
+                ? string.Join(" ", args) + ": " + GetSeverity(index)
+                : "Log type: " + GetSeverity(index));
+        }
+
+        private static string GetSeverity(int index)
+        {
+            return (index % 3) switch
+            {
+                0 => "info",
+                1 => "warning",
+                2 => "error",
+                _ => "info"
+            };
+        }
+    }
+}
---
+++ RabbitMqTest/ReceiveLogsDirect/ReceiveLogsDirect.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLogsDirect
+{
+    internal class ReceiveLogsDirect
+    {
+        private const string ExchangeName = "direct_logs";
+
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: ExchangeName, type: ExchangeType.Direct);
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                    Environment.GetCommandLineArgs()[0]);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            foreach (var severity in args)
+            {
+                channel.QueueBind(queue: queueName,
+                    exchange: ExchangeName,
+                    routingKey: severity);
+            }
+
+            Console.WriteLine(" [*] Waiting for logs.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                var routingKey = ea.RoutingKey;
+                Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
+            };
+
+            channel.BasicConsume(queue: queueName,
+                autoAck: true,
+                consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
