b682b51b7879fc549733174ea12f4d639608f22b
BordeaP <paul.bordea@verivox.com>
2020-04-03 17:57:51 +0300

    Add RPC client and server scenario

---
+++ RabbitMqTest/RPCServer/RPCServer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RPCServer
+{
+    internal class RpcServer
+    {
+        private const string RpcQueue = "rpc_queue";
+
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: RpcQueue, 
+                durable: false,
+                exclusive: false, 
+                autoDelete: false, 
+                arguments: null);
+
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume(
+                queue: RpcQueue,
+                autoAck: false, 
+                consumer: consumer);
+
+            Console.WriteLine(" [x] Awaiting RPC requests");
+
+            consumer.Received += (model, ea) =>
+            {
+                string response = null;
+
+                var body = ea.Body;
+                var props = ea.BasicProperties;
+                var replyProps = channel.CreateBasicProperties();
+                replyProps.CorrelationId = props.CorrelationId;
+
+                try
+                {
+                    var message = Encoding.UTF8.GetString(body);
+                    var n = int.Parse(message);
+                    Console.WriteLine(" [.] fib({0})", message);
+                    response = Fibonacci(n).ToString();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(" [.] " + e.Message);
+                    response = "";
+                }
+                finally
+                {
+                    var responseBytes = Encoding.UTF8.GetBytes(response);
+                    channel.BasicPublish(
+                        exchange: "", 
+                        routingKey: props.ReplyTo,
+                        basicProperties: replyProps, 
+                        body: responseBytes);
+
+                    channel.BasicAck(
+                        deliveryTag: ea.DeliveryTag,
+                        multiple: false);
+                }
+            };
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
---
+++ RabbitMqTest/RpcClient/RpcClient.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RpcClient
+{
+    public class RpcClient
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly string _replyQueueName;
+        private readonly EventingBasicConsumer _consumer;
+        private readonly BlockingCollection<string> _respQueue = new BlockingCollection<string>();
+        private readonly IBasicProperties _props;
+
+        public RpcClient()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _replyQueueName = _channel.QueueDeclare().QueueName;
+            _consumer = new EventingBasicConsumer(_channel);
+
+            _props = _channel.CreateBasicProperties();
+            var correlationId = Guid.NewGuid().ToString();
+            _props.CorrelationId = correlationId;
+            _props.ReplyTo = _replyQueueName;
+
+            _consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var response = Encoding.UTF8.GetString(body);
+                if (ea.BasicProperties.CorrelationId == correlationId)
+                {
+                    _respQueue.Add(response);
+                }
+            };
+        }
+
+        public string Call(string message)
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+
+            _channel.BasicPublish(
+                exchange: "",
+                routingKey: "rpc_queue",
+                basicProperties: _props,
+                body: messageBytes);
+
+            _channel.BasicConsume(
+                consumer: _consumer,
+                queue: _replyQueueName,
+                autoAck: true);
+
+            return _respQueue.Take();
+        }
+
+        public void Close()
+        {
+            _connection.Close();
+        }
+    }
+
+    public class Rpc
+    {
+        public static void Main(string[] args)
+        {
+            var rpcClient = new RpcClient();
+            var value = args.Length > 0 ? args[0] : "20";
+
+            for (var i = 1; i < 30; i++)
+            {
+                Thread.Sleep(200);
+                Console.WriteLine(" [x] Requesting Fibonacci for " + i);
+                var response = rpcClient.Call(i.ToString());
+
+                Console.WriteLine(" [.] Got '{0}'", response);
+            }
+
+            rpcClient.Close();
+        }
+    }
+}
